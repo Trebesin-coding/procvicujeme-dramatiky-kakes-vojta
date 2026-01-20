4d8bba434092343cb43ec995a07df1b26d947898
unknown <kakes122@trebesin.cz>
2026-01-21 00:35:16 +0100

    konec

--- knihovna/knihovna.py
+++ knihovna/knihovna.py
@@ -4,53 +4,44 @@ import os, json
 path = os.path.join(os.path.dirname(__file__), "Knihovna.json")
 
 def main():
-
     with sync_playwright() as p:
-        
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto("https://www.databazeknih.cz")
         page.wait_for_timeout(2000)
 
-        popularos = []
-
-        popularanos = page.locator("div.book").all()
-
-        for item in popularanos[:3]:
+        pop_knihy = []
+        pop_knihy2 = page.locator("div.book").all()
+        for item in pop_knihy2[:3]:
             title = item.locator(".title").inner_text()
             
-
-            popularos.append({
+            pop_knihy.append({
                 "title": title.strip(),
             })
             print(f"Popular bok : {title}")
 
-        new_authoros = []
+        autori = []
+        nova_kniha = page.locator("div.nbox_book a").all()
 
-        new_bok = page.locator("div.nbox_book a").all()
-
-        for i in range (min(3, len(new_bok))):
-            new_bok[i].click()
+        for i in range (min(3, len(nova_kniha))):
+            nova_kniha[i].click()
             page.wait_for_timeout(2000)
 
-            author = page.locator(".author a").inner_text()
-            new_authoros.append(author.strip())
-
-            print(f"New bok au thor: {author}")
+            autor = page.locator(".autor a").inner_text()
+            autori.append(autor.strip())
+            print(f"Autoři: {autor}")
 
             page.go_back()
             page.wait_for_timeout(2000)
 
-
         data = {
-            "popular_books": popularos,
-            "new_books": new_authoros
+            "popular_books": pop_knihy,
+            "new_books": autori
         }
-
         with open (path, mode="w", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
         
-        page.wait_for_timeout(5000)
+        page.wait_for_timeout(3000)
         browser.close()
 
 if __name__ == "__main__":
